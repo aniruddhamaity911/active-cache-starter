e318d6dbc6633b8e7b3bb27ea922bb1ec528e80b
aniruddha <aniruddhamaity911@gmail.com>
2026-08-02 23:45:12 +0530

    first commit

---
+++ src/main/java/io/github/aniruddhamaity911/activecache/ActiveCacheStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.aniruddhamaity911.activecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveCacheStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveCacheStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aniruddhamaity911/activecache/ActiveCacheStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aniruddhamaity911.activecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveCacheStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
